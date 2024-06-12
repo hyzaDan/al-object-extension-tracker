e7d77707486e8b6dd11e62ad37e4478c5b715015
Daniel Hýža <hyza@navisys.cz>
2024-06-12 23:32:08 +0200

    first vsix publish

--- src/extension.ts
+++ src/extension.ts
@@ -9,20 +9,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "al-object-extension-tracker-alfatest" is now active!');
+	console.log('Congratulations, your extension "al-object-extension-tracker" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
     /*
-	const disposable = vscode.commands.registerCommand('al-object-extension-tracker-alfatest.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('al-object-extension-tracker.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from AL Object Extension Tracker AlfaTest!');
 	});
     */
 
-    let disposable = vscode.commands.registerCommand('al-object-extension-tracker-alfatest.searchObjectExtension', () => {
+    let disposable = vscode.commands.registerCommand('al-object-extension-tracker.searchObjectExtension', () => {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
             const document = editor.document;
